3d2423bc2e5204006c764ff53c8cbf36d21c1bd8
junyoungKim <wnsdud1900427@gmail.com>
2026-02-16 12:21:49 +0900

    refactor: 불필요한 테스트 케이스 제거

--- apps/commerce-api/src/test/java/com/loopers/user/service/UserServiceTest.java
+++ apps/commerce-api/src/test/java/com/loopers/user/service/UserServiceTest.java
@@ -34,51 +34,6 @@ public class UserServiceTest {
     private UserService userService;
 
     @Test
-    void 정상_입력시_회원가입_성공() {
-        //given
-        CreateUserRequest request = new CreateUserRequest(
-                "testId", "password123!", "김준영", "1990-04-27", "test@test.com"
-        );
-
-        given(passwordEncoder.encode(request.password())).willReturn("encodedPassword");
-        given(userRepository.save(any(User.class)))
-                .willAnswer(invocation -> invocation.getArgument(0));
-
-        //when
-        User user = userService.createUser(request);
-
-        //then
-        assertThat(user.getLoginId()).isEqualTo(request.loginId());
-        verify(userRepository).save(any(User.class));
-    }
-
-    @Test
-    void 비밀번호가_암호화되어_저장된다() {
-        //given
-        String rawPassword = "password123!";
-        String encodedPassword = "encoded_password_hash";
-        CreateUserRequest request = new CreateUserRequest(
-                "testId", rawPassword, "test", "1990-04-27", "test@test.com"
-        );
-
-        //password를 암호화한다.
-        given(passwordEncoder.encode(rawPassword)).willReturn(encodedPassword);
-        //사용자를 저장소에 저장한다.
-        given(userRepository.save(any(User.class)))
-                .willAnswer(invocation -> invocation.getArgument(0));
-
-        //when
-        //회원가입을 진행했을 때
-        User user = userService.createUser(request);
-
-        //then
-        //비밀번호가 암호화되었는지 확인한다.
-        assertThat(user.getPassword()).isEqualTo(encodedPassword);
-        //암호화 로직 호출했는지 확인한다.
-        verify(passwordEncoder).encode(rawPassword);
-    }
-
-    @Test
     void 유효한_로그인ID로_내_정보를_조회한다() {
         // given
         String loginId = "testId";
