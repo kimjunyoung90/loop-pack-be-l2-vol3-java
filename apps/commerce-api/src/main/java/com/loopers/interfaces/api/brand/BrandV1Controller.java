29efdec5edeab44aa25b408d7c5204c5f01b1e49
junyoungKim <wnsdud1900427@gmail.com>
2026-02-23 20:03:54 +0900

    feat: Brand 사용자 목록 조회 API 구현

--- apps/commerce-api/src/main/java/com/loopers/interfaces/api/brand/BrandV1Controller.java
+++ apps/commerce-api/src/main/java/com/loopers/interfaces/api/brand/BrandV1Controller.java
@@ -4,10 +4,9 @@ import com.loopers.application.brand.BrandInfo;
 import com.loopers.application.brand.BrandService;
 import com.loopers.interfaces.api.ApiResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,6 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class BrandV1Controller implements BrandV1ApiSpec {
 
     private final BrandService brandService;
+
+    @GetMapping
+    @Override
+    public ApiResponse<Page<BrandV1Dto.GetBrandResponse>> getBrands(
+            @RequestParam int page,
+            @RequestParam int size
+    ) {
+        Page<BrandInfo> brandInfo = brandService.getBrands(PageRequest.of(page, size));
+        return ApiResponse.success(brandInfo.map(BrandV1Dto.GetBrandResponse::from));
+    }
 
     @GetMapping("/{brandId}")
     @Override
